32a4a5556d815170349bda26747ffba8d917518b
Erick Ruiz <erickuriel238@gmail.com>
2024-09-10 13:32:28 -0600

    Refactor CSS styles and fix typo in Forecast component

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -26,7 +26,7 @@ const Forecast = ({ data }: Props): JSX.Element => {
   return (
     <div
       className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full
-     lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg"
+     lg:h-auto bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg"
     >
       <div className="mx-auto w-[300px] m-10 ">
         <section className="text-center">
