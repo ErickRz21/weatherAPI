5d59b1bc21285ef24875588dd320c4ac21db5493
Erick Ruiz <erickuriel238@gmail.com>
2024-09-18 13:57:57 -0600

    Refactor CSS styles in Search component for improved responsiveness

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -17,7 +17,7 @@ const Search = ({
   onSubmit,
 }: Props): JSX.Element => {
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-cyan-400 to-rose-400 h-[100vh] w-full">
+    <main className="flex justify-center items-center bg-gradient-to-br from-cyan-400 to-rose-400 h-[93vh] md:h-[100vh] w-full">
       <section
         className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full
        lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded lg:rounded-3xl drop-shadow-lg text-zinc-700"
