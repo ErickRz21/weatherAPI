d655db8b3c4ab23d106c0e95d407aea9b9c5eb51
Erick Ruiz <erickuriel238@gmail.com>
2024-09-19 00:59:59 -0600

    Refactor Loading component styles for improved visual consistency and performance

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,5 @@
 const LoadingScreen = () => (
-  <div className="fixed inset-0 flex space-x-2 justify-center items-center bg-black/10 z-50 ease-in-out duration-300">
+  <div className="fixed inset-0 flex space-x-2 justify-center items-center bg-black/5 z-50">
     <span className="sr-only">Loading...</span>
     <div className="h-6 w-6 bg-white rounded-full animate-bounce [animation-delay:-0.3s]"></div>
     <div className="h-6 w-6 bg-white rounded-full animate-bounce [animation-delay:-0.15s]"></div>
